a86f7b912aa7b324c59776086ac5167e6756f3c5
Igor_ <igrmartoli@gmail.com>
2022-09-16 12:17:17 -0300

    starting socket io

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express();
+const http = require('http').createServer(app)
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+    socket.on('Boas vindas', (data) => {
+        console.log(data)
+    })
+
+    socket.on('palavra', (data) => {
+        console.log(data)
+        socket.emit('resultado', data.palavra+" Guia do programador")
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`X desconectou`)
+    })
+})
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+
+
+http.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000')
+})
